ab83a2bbe774b23db76f47f3819f66dd80a0af51
Jhon Kenieth Yote amarila <jhonkennethamarila6@gmail.com>
2026-07-25 03:46:27 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -15,13 +15,9 @@ try {
         [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::ATTR_EMULATE_PREPARES   => true,
         ]
     );
-
-    // Set timezone
-    $pdo->exec("SET TIME ZONE 'Asia/Manila';");
-
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
